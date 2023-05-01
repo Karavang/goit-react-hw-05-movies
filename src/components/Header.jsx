76313164b6ebb59f4b6c0bb2b9a678259a728a29
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-05-01 20:08:43 +0200

    first and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <ul className="navbar-nav list-header">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/movies">
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
