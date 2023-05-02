80a3b73602de0f4ba89dab6d43ffcdbbd65f8e06
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-05-02 13:44:22 +0200

    changes for structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { Header } from './Header';
 
 import { Home } from './Home';
 import { Movies } from './Movies';
@@ -6,6 +7,7 @@ import { Movies } from './Movies';
 export const App = () => {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Header } from './Header';
 
 export const Home = () => {
-  return <Header />;
+  return <h1>aboba</h1>;
 };
