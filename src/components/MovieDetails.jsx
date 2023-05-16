33b4b44dc46fce21d11482e81ace96133c14b4f8
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-05-16 14:22:35 +0200

    info

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -7,55 +7,56 @@ export const MovieDetails = () => {
   const { id } = useParams();
   console.log(id);
   const [film, setFilm] = useState();
-
   useEffect(() => {
-    const film = fullInfo(id);
-    setFilm(film);
-  }, []);
+    const fetchFilm = async () => {
+      const film = await fullInfo(id);
+      setFilm(film);
+    };
 
-  return (
-    <div>
-      <Link to="/movies" className="li-of-titles">
-        <button>Go back</button>
-      </Link>
-      <ul className="flexator">
-        <li>
-          <img src="/" alt="" />
-        </li>
-        <li>
-          <ul className="list-info">
-            <li>
-              <ul className="info-l">
-                <li>
-                  <h1>abboa</h1>
-                </li>
-                <li>
-                  <p>Score</p>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <ul className="info-l">
-                <li>
-                  <h2>Overview</h2>
-                </li>
-                <li>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Aliquam eius quam mollitia explicabo iure! Vero corporis natus
-                  laboriosam, mollitia aliquid nulla quam molestias
-                  necessitatibus cumque illum vitae atque itaque! Quos.
-                </li>
-              </ul>
-            </li>
-            <li>
-              <ul className="info-l">
-                <li>Genres</li>
-                <li>Drama</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  );
+    fetchFilm();
+  });
+  console.log(film);
+  if (film) {
+    return (
+      <div>
+        <Link to="/movies" className="li-of-titles">
+          <button>Go back</button>
+        </Link>
+        <ul className="flexator">
+          <li>
+            <img src="/" alt="" />
+          </li>
+          <li>
+            <ul className="list-info">
+              <li>
+                <ul className="info-l">
+                  <li>
+                    <h1>{film.original_title}</h1>
+                  </li>
+                  <li>
+                    <p>{film.vote_average}</p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul className="info-l">
+                  <li>
+                    <h2>Overview</h2>
+                  </li>
+                  <li>{film.overview}</li>
+                </ul>
+              </li>
+              <li>
+                <ul className="info-l">
+                  {film.genres.map(e => (
+                    <li>{e.name}</li>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 };
